e42e7a37da4bcf078bb3e3afab8e900169268574
yasmen tarek <81187025+yasmen1@users.noreply.github.com>
2022-10-18 20:54:43 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "holberton.h"
 #include <stdlib.h>
 
 /**
@@ -10,20 +10,31 @@
 static int (*check_for_specifiers(const char *format))(va_list)
 {
 	unsigned int i;
-	print_typ p[] = {
-		{"_char", printf_char},
-		{"_str",  printf_str},
+	print_t p[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"i", print_i},
+		{"d", print_d},
+		{"u", print_u},
+		{"b", print_b},
+		{"o", print_o},
+		{"x", print_x},
+		{"X", print_X},
+		{"p", print_p},
+		{"S", print_S},
+		{"r", print_r},
+		{"R", print_R},
 		{NULL, NULL}
 	};
 
-	for (i = 0; p[i].typ != NULL; i++)
+	for (i = 0; p[i].t != NULL; i++)
 	{
-		if (*(p[i].typ) == *format)
+		if (*(p[i].t) == *format)
 		{
 			break;
 		}
 	}
-	return (p[i].func);
+	return (p[i].f);
 }
 
 /**
@@ -36,7 +47,7 @@ int _printf(const char *format, ...)
 {
 	unsigned int i = 0, count = 0;
 	va_list valist;
-	int (*func)(va_list);
+	int (*f)(va_list);
 
 	if (format == NULL)
 		return (-1);
