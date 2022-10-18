04576948af2cf5d6793b24d78d0345308a299c44
Mohamed Salah <m.salah@mu.edu.eg>
2022-10-18 11:58:51 -0700

    update printf_chars

--- printf_chars.c
+++ printf_chars.c
@@ -2,29 +2,29 @@
 #include <stdlib.h>
 
 /**
- * printf_char - prints a char
- * @_char: char to print
+ * print_c - prints a char
+ * @c: char to print
  *
  * Return: always 1
  */
-int printf_char(va_list _char)
+int print_c(va_list c)
 {
-	char ch = (char)va_arg(_char, int);
+	char ch = (char)va_arg(c, int);
 
 	_putchar(ch);
 	return (1);
 }
 
 /**
- * printf_str - prints a string
- * @_str: string to print
+ * print_s - prints a string
+ * @s: string to print
  *
  * Return: number of chars printed
  */
-int printf_str(va_list _str)
+int print_s(va_list s)
 {
 	int count;
-	char *str = va_arg(_str, char *);
+	char *str = va_arg(s, char *);
 
 	if (str == NULL)
 		str = "(null)";
@@ -34,20 +34,21 @@ int printf_str(va_list _str)
 	}
 	return (count);
 }
+
 /**
  * hex_print - prints a char's ascii value in uppercase hex
- * @_char: char to print
+ * @c: char to print
  *
  * Return: number of chars printed (always 2)
  */
-static int hex_print(char _char)
+static int hex_print(char c)
 {
 	int count;
 	char diff = 'A' - ':';
 	char d[2];
 
-	d[0] = _char / 16;
-	d[1] = _char % 16;
+	d[0] = c / 16;
+	d[1] = c % 16;
 	for (count = 0; count < 2; count++)
 	{
 		if (d[count] >= 10)
@@ -59,16 +60,16 @@ static int hex_print(char _char)
 }
 
 /**
- * printf_STR - prints a string and nonprintable character ascii values
- * @_STR: string to print
+ * print_S - prints a string and nonprintable character ascii values
+ * @S: string to print
  *
  * Return: number of chars printed
  */
-int printf_STR(va_list _STR)
+int print_S(va_list S)
 {
 	unsigned int i;
 	int count = 0;
-	char *str = va_arg(_STR, char *);
+	char *str = va_arg(S, char *);
 
 	if (str == NULL)
 		str = "(null)";
@@ -91,17 +92,17 @@ int printf_STR(va_list _STR)
 }
 
 /**
- * printf_rev - prints astring in reverse
- * @_rev: string to print
+ * print_r - prints astring in reverse
+ * @r: string to print
  *
  * Return: number of chars printed
  */
-int printf_rev(va_list _rev)
+int print_r(va_list r)
 {
 	char *str;
 	int i, count = 0;
 
-	str = va_arg(_rev, char *);
+	str = va_arg(r, char *);
 	if (str == NULL)
 		str = ")llun(";
 	for (i = 0; str[i]; i++)
